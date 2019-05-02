0affcf4419c656383ad81081e2bdd3f081e087c7
denghe <denghe@hotmail.com>
2019-05-02 13:49:46 +0800

    add some assert

--- xxlib/xx_uv.h
+++ xxlib/xx_uv.h
@@ -1155,7 +1155,10 @@ namespace xx {
 				auto&& p = xx::TryMake<UvKcpPeerBase>(uv);
 				if (!p) return -1;								// not enough memory
 				peer_w = p;										// bind
-				p->udp = As<UvKcp>(shared_from_this());
+				auto&& self = As<UvKcp>(shared_from_this());
+				xx::CoutN("1: ", self.use_count());
+				p->udp = self;
+				xx::CoutN("2: ", self.use_count());
 				memcpy(&p->conv, recvBuf + 4, 4);
 				memcpy(&p->addr, addr, sizeof(sockaddr_in6));	// upgrade peer's tar addr
 				p->createMS = NowSteadyEpochMS();
@@ -1165,6 +1168,7 @@ namespace xx {
 				p->Flush();
 				connected = true;								// set flag
 				owner->Accept(p);								// cleanup all reqs
+				xx::CoutN("3: ", self.use_count());
 				return 0;
 			}
 
